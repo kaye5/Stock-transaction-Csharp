20d9fba2d779e10ad7acf6c32f31b96dda6939ec
kaye5 <kevinyusyus@gmail.com>
2019-10-27 00:53:06 +0700

    first commit

---
+++ practice1/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace practice1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Import = new System.Windows.Forms.Button();
+            this.stockTable = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.keteranganTbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabDetailSave = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.qtyTbox = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stockTableDetail = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.priceTbox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.qtyTbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(625, 332);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Click += new System.EventHandler(this.tabControl1_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.Import);
+            this.tabPage1.Controls.Add(this.stockTable);
+            this.tabPage1.Controls.Add(this.buttonSave);
+            this.tabPage1.Controls.Add(this.keteranganTbox);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(617, 306);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tab Header";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Import
+            // 
+            this.Import.Location = new System.Drawing.Point(309, 105);
+            this.Import.Name = "Import";
+            this.Import.Size = new System.Drawing.Size(97, 23);
+            this.Import.TabIndex = 5;
+            this.Import.Text = "Import Header";
+            this.Import.UseVisualStyleBackColor = true;
+            this.Import.Click += new System.EventHandler(this.Import_Click);
+            // 
+            // stockTable
+            // 
+            this.stockTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader1});
+            this.stockTable.FullRowSelect = true;
+            this.stockTable.GridLines = true;
+            this.stockTable.Location = new System.Drawing.Point(10, 81);
+            this.stockTable.Name = "stockTable";
+            this.stockTable.Size = new System.Drawing.Size(293, 218);
+            this.stockTable.TabIndex = 4;
+            this.stockTable.UseCompatibleStateImageBehavior = false;
+            this.stockTable.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "name";
+            this.columnHeader1.Width = 102;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(228, 32);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // keteranganTbox
+            // 
+            this.keteranganTbox.Location = new System.Drawing.Point(85, 34);
+            this.keteranganTbox.Name = "keteranganTbox";
+            this.keteranganTbox.Size = new System.Drawing.Size(137, 20);
+            this.keteranganTbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Keterangan : ";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tabDetailSave);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.qtyTbox);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.stockTableDetail);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.priceTbox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(617, 306);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tab Detail";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabDetailSave
+            // 
+            this.tabDetailSave.Location = new System.Drawing.Point(251, 58);
+            this.tabDetailSave.Name = "tabDetailSave";
+            this.tabDetailSave.Size = new System.Drawing.Size(75, 23);
+            this.tabDetailSave.TabIndex = 8;
+            this.tabDetailSave.Text = "Save";
+            this.tabDetailSave.UseVisualStyleBackColor = true;
+            this.tabDetailSave.Click += new System.EventHandler(this.tabDetailSave_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(101, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(118, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // qtyTbox
+            // 
+            this.qtyTbox.Location = new System.Drawing.Point(101, 61);
+            this.qtyTbox.Name = "qtyTbox";
+            this.qtyTbox.Size = new System.Drawing.Size(120, 20);
+            this.qtyTbox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Quantity";
+            // 
+            // stockTableDetail
+            // 
+            this.stockTableDetail.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.stockTableDetail.FullRowSelect = true;
+            this.stockTableDetail.GridLines = true;
+            this.stockTableDetail.Location = new System.Drawing.Point(6, 98);
+            this.stockTableDetail.Name = "stockTableDetail";
+            this.stockTableDetail.Size = new System.Drawing.Size(608, 201);
+            this.stockTableDetail.TabIndex = 4;
+            this.stockTableDetail.UseCompatibleStateImageBehavior = false;
+            this.stockTableDetail.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ID";
+            this.columnHeader3.Width = 68;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Stock ID";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Header ID";
+            this.columnHeader5.Width = 73;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Price";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Quantity";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Transaction Name";
+            this.columnHeader8.Width = 109;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Stock Name";
+            this.columnHeader9.Width = 102;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Stock";
+            // 
+            // priceTbox
+            // 
+            this.priceTbox.Location = new System.Drawing.Point(101, 34);
+            this.priceTbox.Name = "priceTbox";
+            this.priceTbox.Size = new System.Drawing.Size(117, 20);
+            this.priceTbox.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Export data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 335);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.qtyTbox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox keteranganTbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox priceTbox;
+        private System.Windows.Forms.ListView stockTable;
+        private System.Windows.Forms.ListView stockTableDetail;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown qtyTbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button tabDetailSave;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button Import;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ practice1/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+namespace practice1
+{
+    public partial class Form1 : Form
+    {
+        String headerID;
+        MySqlConnection connection = new MySqlConnection("Server=localhost;Database=practice1;Uid=root;Pwd=vroxine;");
+        String sql;
+        MySqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                connection.Open();
+                getHeaderData();
+            }
+            catch (MySqlException err)
+            {
+                MessageBox.Show(err.Message, err.Number.ToString());
+            }
+        }
+
+        private void getHeaderData()
+        {
+            try
+            {
+                if (stockTable.Items.Count > 0)
+                    stockTable.Items.Clear();
+                sql = $"select * from practice1.header";
+                cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ListViewItem item = new ListViewItem(reader.GetInt16(0).ToString());
+                    item.SubItems.Add(reader.GetString(1));
+                    stockTable.Items.Add(item);
+                }
+                cmd.Dispose();
+                reader.Dispose();
+                if (stockTableDetail.Items.Count > 0)
+                    stockTableDetail.Items.Clear();
+                
+            }
+            catch (MySqlException err)
+            {
+                MessageBox.Show(err.Message, err.Number.ToString());
+            }
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(keteranganTbox.Text))
+                {
+                    sql = $"insert into header(`name`) values('{keteranganTbox.Text}')";
+                    cmd = new MySqlCommand(sql, connection);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    getHeaderData();
+                }
+            }
+            catch (MySqlException err)
+            {
+                MessageBox.Show(err.Message, err.Number.ToString());
+            }
+        }
+
+        private void tabDetailShow()
+        {
+            try
+            {
+                sql = $"select name from stock";
+                cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())                
+                    comboBox1.Items.Add(reader.GetString(0));
+                
+                cmd.Dispose();
+                reader.Dispose();
+            }
+            catch (MySqlException err)
+            {
+                MessageBox.Show(err.Message, err.Number.ToString());
+            }
+        }
+
+        private void tabControl1_Click(object sender, EventArgs e)
+        {
+            if(tabControl1.SelectedIndex == 1)
+            {
+                if (stockTable.SelectedItems.Count > 0)
+                {
+                    tabControl1.SelectTab(1);
+                    headerID = stockTable.SelectedItems[0].Text;
+                    Console.WriteLine(headerID);
+                    tabDetailShow();
+                    getTransactionData();
+                } else
+                {
+                    MessageBox.Show("No item selected");
+                    tabControl1.SelectTab(0);
+                }
+            }
+        }
+
+        private void tabDetailSave_Click(object sender, EventArgs e)
+        {
+            int thisQty = Convert.ToInt16(qtyTbox.Value);
+            int thisPrice = Convert.ToInt16(priceTbox.Text);
+            int hid = Convert.ToInt16(headerID);
+            int sid = comboBox1.SelectedIndex+1;
+            sql = "insert into transaction(`sid`,`hid`,`price`,`qty`) " + 
+                  $"values({sid},{hid},{thisPrice},{thisQty})"  ;
+            Console.WriteLine(sql);
+            cmd = new MySqlCommand(sql, connection);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            getTransactionData();
+        }
+
+        private void getTransactionData()
+        {
+            if (stockTableDetail.Items.Count > 0)
+                stockTableDetail.Items.Clear();
+            sql = "SELECT t1.*  , t2.`name` , t3.`name` FROM `transaction` t1 INNER JOIN `header` t2 on t1.hid = t2.id INNER JOIN `stock` t3 on t1.sid = t3.id";
+            cmd = new MySqlCommand(sql, connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                ListViewItem item = new ListViewItem(reader.GetInt16(0).ToString());
+                for (int i = 1; i <= 6; i++)
+                {
+                    item.SubItems.Add(reader.GetValue(i).ToString());
+                }
+                stockTableDetail.Items.Add(item);
+            }
+            reader.Dispose();
+            cmd.Dispose();
+        }
+
+        private void readFile(String filePath)
+        {
+            byte[] buffer;
+            FileInfo fi = new FileInfo(filePath);
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            try
+            {
+                int length = (int)fileStream.Length;  
+                buffer = new byte[length];            
+                int count;                            
+                int sum = 0;     
+                while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
+                    sum += count;  
+            }
+            finally
+            {
+                fileStream.Close();
+            }
+            String fileText = Encoding.UTF8.GetString(buffer);            
+            string[] splitText = fileText.Split(new[] { Environment.NewLine },StringSplitOptions.None);
+            List<string> lines = splitText.ToList<string>();
+            if (lines[0] == "ADD HEADER:")
+            {
+                lines.RemoveAt(0);
+                foreach (string el in lines)
+                {
+                    sql = $"insert into header(`name`) values('{el}')";
+                    cmd = new MySqlCommand(sql, connection);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                }
+            }
+            else MessageBox.Show("Wrong File", "ERROR");
+            getHeaderData();
+        }
+
+        private void writeFIle(String path)
+        {
+            FileStream fs = File.Create(path + @"\header.txt");
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                sql = "select * from header";
+                cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                
+                writer.WriteLine("ID,NAME");
+                while (reader.Read())
+                {
+                    writer.WriteLine($"{reader.GetInt16(0).ToString()},{reader.GetString(1)}");
+                }
+                reader.Dispose();
+                cmd.Dispose();
+            }
+            fs.Dispose();
+            fs = File.Create(path + @"\stock.txt");
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                sql = "select * from stock";
+                cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                writer.WriteLine("ID,NAME");
+                while (reader.Read())
+                {
+                    writer.WriteLine($"{reader.GetInt16(0).ToString()},{reader.GetString(1)}");
+                }
+                reader.Dispose();
+                cmd.Dispose();
+            }
+            fs.Dispose();
+            fs = File.Create(path + @"\transaction.txt");
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                sql = "select * from transaction";
+                cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                writer.WriteLine("ID,StockID,HeaderID,Price,Quantity");
+                while (reader.Read())
+                {
+                    for (int i = 0; i <= 4; i++)
+                        writer.Write(reader.GetValue(i).ToString()+",");
+                    writer.WriteLine();
+                }
+                reader.Dispose();
+                cmd.Dispose();
+            }
+        }
+
+        private void Import_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.ShowDialog();
+            readFile(openFile.FileName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            string folderPath = folderBrowserDialog1.SelectedPath;
+            writeFIle(folderPath);
+            
+        }
+    }
+}
